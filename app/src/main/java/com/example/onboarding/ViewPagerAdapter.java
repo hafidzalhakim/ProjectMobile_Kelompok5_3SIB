7f15f94bd1bfedd6885e6ae3b9e5dba476c2e96e
Hafidz Alhakim <hafidz21si@mahasiswa.pcr.ac.id>
2023-12-26 11:30:51 +0700

    Penyelesaian pada ViewPagerAdapter

---
+++ app/src/main/java/com/example/onboarding/ViewPagerAdapter.java
@@ -0,0 +1,72 @@
+package com.example.onboarding;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    Context context;
+
+    int images[] = {
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image4
+    };
+
+    int heading[] = {
+          R.string.heading_one,
+          R.string.heading_two,
+          R.string.heading_three,
+          R.string.heading_fourth
+    };
+
+    int description[] = {
+          R.string.desc_one,
+          R.string.desc_two,
+          R.string.desc_three,
+          R.string.desc_fourth
+    };
+
+    public ViewPagerAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return heading.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (LinearLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int postion){
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slider_layout, container, false);
+
+        ImageView slidetitleimage = (ImageView) view.findViewById(R.id.titleImage);
+        TextView slideHeading = (TextView) view.findViewById(R.id.textTitle);
+        TextView slideDescription = (TextView) view.findViewById(R.id.textDescription);
+
+        slidetitleimage.setImageResource(images[postion]);
+        slideHeading.setText(heading[postion]);
+        slideDescription.setText(description[postion]);
+
+        container.addView(view);
+
+        return view;
+    }
+}
